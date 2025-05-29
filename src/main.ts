3e2db7d80b6e2e3d72a57947d89a5fc642bac6ae
Imma <imma.becker@outlook.de>
2025-05-29 16:19:32 +0200

    readded permissions

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+// import { setTheme } from '@fluentui/web-components';
+// import { webLightTheme } from '@fluentui/tokens';
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
+
+// setTheme(webLightTheme);
