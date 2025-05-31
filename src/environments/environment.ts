1bd67a13b145f33325c5e5974b9d16119bc21478
Imma <imma.becker@outlook.de>
2025-05-31 19:19:57 +0200

    Refactor deployment configuration and environment settings for Angular app

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
     supabaseUrl: 'https://bfozbjjsbwakbucgdpao.supabase.co',
     supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJmb3piampzYndha2J1Y2dkcGFvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDg0Njc1NTYsImV4cCI6MjA2NDA0MzU1Nn0.BPz5PAlcFygo7R-Smcedug12OqfR_fGsxAbkGOpHh1M',
-    production: false
+    production: false,
+    baseHref: '/'
 };
