23f06fcd5a458ae18afe0ab9fb14b4ec26f485af
Imma <imma.becker@outlook.de>
2025-06-25 22:03:44 +0200

    chore: update Angular dependencies to version 20.0.5 and Supabase to version 2.50.2
    feat: add RichTextComponent for rich text editing with clipboard functionality

---
+++ src/app/shared/form/rich-text/rich-text.ts
@@ -0,0 +1,70 @@
+import { Component, ElementRef, input, signal, viewChild } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { copyToClipboard } from '../../utils/clipboard-utils';
+import ButtonComponent from "../button/button.component";
+import { getProviders, InputBaseComponent } from '../shared/input-base';
+import InputLabelComponent from "../shared/input-label";
+import { TiptapEditorDirective } from 'ngx-tiptap';
+import { Editor } from '@tiptap/core';
+
+@Component({
+    selector: 'app-rich-text',
+    template: `
+        <label class="column">
+            <app-input-label/>
+            <tiptap-editor [editor]="editor" [(ngModel)]="value"></tiptap-editor>
+            <div class="form-input">
+                <input #input [required]="true" type="text"
+                    [(ngModel)]="value" [attr.disabled]="disabledState()"
+                    [placeholder]="placeholder()" [pattern]="pattern()"
+                    [autocomplete]="autocomplete()" (click)="onClick($event)"
+                    (input)="emitChange()"
+                    (keypress)="onKeyPress($event)">
+                @if (copyable()) {
+                    <app-button type="subtle" [icon]="copied() ? 'checkmark' : 'copy'"
+                        class="icon-only input-btn" (onClick)="copy()"/>
+                }
+            </div>
+        </label>
+    `,
+    providers: getProviders(() => RichTextComponent),
+    imports: [FormsModule, InputLabelComponent, ButtonComponent, TiptapEditorDirective]
+})
+export class RichTextComponent extends InputBaseComponent<string> {
+    
+    readonly characterLimit = input<number>(0);
+    readonly autocomplete = input<string>('off');
+    readonly pattern = input<string | RegExp>('');
+    readonly patternErrorMsg = input<string>();
+    readonly copyable = input(false);
+
+    protected readonly copied = signal(false);
+    protected readonly editor = new Editor({
+        extensions: [],
+    });
+
+
+    private readonly inputView = viewChild.required('input', { read: ElementRef });
+
+    protected onClick(event: MouseEvent) {
+        event.stopImmediatePropagation();
+    }
+
+    protected onKeyPress(event: KeyboardEvent) {
+        if (event.key === 'Enter')
+            this.inputView().nativeElement.blur();
+        if (this.characterLimit() && (this.value()?.length ?? 0) >= this.characterLimit()) {
+            event.preventDefault();
+        }
+    }
+
+
+    protected async copy() {
+        const value = this.value();
+        if (!value) return;
+        copyToClipboard(value);
+        this.copied.set(true);
+        await new Promise(resolve => setTimeout(resolve, 3000));
+        this.copied.set(false);
+    }
+}
