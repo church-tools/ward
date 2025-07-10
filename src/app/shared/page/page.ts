999f9ec1de26366f42214c97cf897a43cf2243d1
Imma <imma.becker@outlook.de>
2025-07-10 18:35:21 +0200

    migrated page animation system

--- src/app/shared/page/page.ts
+++ src/app/shared/page/page.ts
@@ -1,12 +1,75 @@
-import { Component } from '@angular/core';
+import { afterNextRender, ApplicationRef, Component, ElementRef, inject, signal, ViewContainerRef } from '@angular/core';
+
+export const LEAVE_ANIMATION = 'page-leave-animation';
 
 @Component({
     selector: 'app-page',
     template: ``,
     host: {
-        class: "column gap-3",
-    },
+        class: "page column gap-3",
+        '[class.page-enter-animation]': 'entering()',
+    }
 })
 export abstract class PageComponent {
+
+    private readonly renderer = (inject(ViewContainerRef) as any)._hostLView?.[11] as { delegate?: any, data?: any };
+    private readonly el = inject(ElementRef).nativeElement;
+    private readonly app = inject(ApplicationRef);
+
+    protected readonly entering = signal(true);
+
+    constructor() {
+        setTimeout(() => this.entering.set(false), 350);
+        if (!this.renderer) return;
+        const renderer = this.renderer.delegate || this.renderer;
+        const { removeChild, data } = renderer;
+
+        if (data[LEAVE_ANIMATION]) {
+            data[LEAVE_ANIMATION].push(this.el);
+            return;
+        }
+
+        data[LEAVE_ANIMATION] = [this.el];
+
+        afterNextRender(() => {
+            renderer.removeChild = (parent: Node, el: Node, host?: boolean) => {
+                const remove = (): void => removeChild.call(renderer, parent, el, host);
+                const elements: Element[] = data[LEAVE_ANIMATION];
+                const element = elements.find(leave => el.contains(leave));
+
+                if (!element) {
+                    remove();
+                    return;
+                }
+                this.entering.set(false);
+
+                const { length } = element.getAnimations();
+
+                elements.splice(elements.indexOf(element), 1);
+                element.classList.add(LEAVE_ANIMATION);
+
+                const animations = element.getAnimations();
+                const last = animations.at(-1);
+                const finish = (): void => {
+                    if (!parent || parent.contains(el)) {
+                        remove();
+                        this.app.tick();
+                    }
+                };
+
+                if (animations.length > length && last) {
+                    last.onfinish = finish;
+                    last.oncancel = finish;
+                } else {
+                    remove();
+                }
+            };
+        });
+    }
     
+    public ngOnDestroy(): void {
+        const data = this.renderer?.data || { [LEAVE_ANIMATION]: [] };
+        setTimeout(() => data[LEAVE_ANIMATION] = data[LEAVE_ANIMATION].filter((e: Element) => e !== this.el));
+    }
+
 }
--- src/app/shared/shell/shell.ts
+++ src/app/shared/shell/shell.ts
@@ -1,50 +1,9 @@
-import { animate, group, query, style, transition, trigger } from '@angular/animations';
 import { AfterViewInit, Component, inject } from '@angular/core';
-import { ChildrenOutletContexts } from '@angular/router';
-import { easeIn, easeOut } from '../utils/style';
 import WindowService from '../window.service';
-
-export const pageAnimations = trigger('routeAnimations', [
-    transition('* <=> *', [
-        query(':enter, :leave', [
-            style({
-                position: 'absolute',
-            })
-        ], { optional: true }),
-        query(':enter .animated, :leave .animated', [
-            style({ 
-                animation: 'none !important',
-                transform: 'none',
-                opacity: 1
-            })
-        ], { optional: true }),
-        query(':enter', [
-            style({
-                opacity: 0,
-                transform: 'translateY(100px)'
-            })
-        ], { optional: true }),
-        group([
-            query(':leave', [
-                animate(`150ms ${easeIn}`, style({
-                    opacity: 0,
-                    // transform: 'scale(0.98)'
-                }))
-            ], { optional: true }),
-            query(':enter', [
-                animate(`300ms 100ms ${easeOut}`, style({
-                    opacity: 1,
-                    transform: 'translateY(0)'
-                }))
-            ], { optional: true })
-        ])
-    ])
-]);
 
 @Component({
     selector: 'app-shell',
     template: '',
-    animations: [pageAnimations],
     host: {
         '[class.focused]': 'windowService.focused()',
         '[class.narrow]': 'windowService.isSmall()',
@@ -53,11 +12,6 @@ export const pageAnimations = trigger('routeAnimations', [
 export abstract class ShellComponent implements AfterViewInit {
 
     protected readonly windowService = inject(WindowService);
-    private readonly contexts = inject(ChildrenOutletContexts);
-
-    protected getRouteAnimationData() {
-        return this.contexts.getContext('primary')?.route?.snapshot?.data?.['animation'];
-    }
 
     ngAfterViewInit() {
         window.dispatchEvent(new CustomEvent('view-initialized'));
