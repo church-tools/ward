147524c8d285d85b4a4733cc86b652fb1910fead
Imma <imma.becker@outlook.de>
2025-07-12 20:14:53 +0200

    remove github pages

--- src/app/shared/utils/url-utils.ts
+++ src/app/shared/utils/url-utils.ts
@@ -2,6 +2,6 @@ import { environment } from "../../../environments/environment";
 
 export function getSiteOrigin() {
     return environment.production
-        ? window.location.origin + '/ward'
+        ? window.location.origin
         : 'http://localhost:4200';
 }
