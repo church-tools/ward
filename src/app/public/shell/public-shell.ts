0dd42e25611a3160bddfa26b6f6a74737516034f
Imma <imma.becker@outlook.de>
2025-07-23 18:25:37 +0200

    feat: implement service worker for PWA functionality and update site metadata

--- src/app/public/shell/public-shell.ts
+++ src/app/public/shell/public-shell.ts
@@ -1,12 +1,13 @@
 import { Component, inject } from '@angular/core';
 import { Router, RouterOutlet } from '@angular/router';
+import { TranslateModule } from '@ngx-translate/core';
 import { ShellComponent } from '../../shared/shell/shell';
 
 @Component({
     selector: 'app-public-shell',
     templateUrl: './public-shell.html',
     styleUrls: ['../../shared/shell/shell.scss', './public-shell.scss'],
-    imports: [RouterOutlet],
+    imports: [RouterOutlet, TranslateModule],
 })
 export class PublicShellComponent extends ShellComponent {
 
