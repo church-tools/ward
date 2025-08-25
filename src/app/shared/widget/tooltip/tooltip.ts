142d33c5101f03a9d50fe3eb7616ea56163f9433
Imma <imma.becker@outlook.de>
2025-08-25 19:59:56 +0200

    tooltip refactor

--- src/app/shared/widget/tooltip/tooltip.ts
+++ src/app/shared/widget/tooltip/tooltip.ts
@@ -1,30 +1,38 @@
-import { Component, input } from '@angular/core';
-import { Icon, IconComponent } from '../../icon/icon';
+import { Component, input, signal } from '@angular/core';
+import { Icon, IconComponent, IconSize } from '../../icon/icon';
 import { ColorName } from '../../utils/color-utils';
-import { TooltipPopupComponent } from './tooltip-popup';
+import { wait } from '../../utils/flow-control-utils';
 
 @Component({
     selector: 'app-tooltip',
-    imports: [IconComponent, TooltipPopupComponent],
+    imports: [IconComponent],
     template: `
-        <app-icon [size]="size()" [icon]="icon()" [class]="color()"/>
-        <app-tooltip-popup><ng-content/></app-tooltip-popup>`,
-    styles: [`
-        :host {
-            position: relative;
-            display: inline-block;
-            align-self: center;
-        }
-        app-icon {
-            font-size: 14px !important;
-            min-width: 14px !important;
-            min-height: 14px !important;
-        }
-    `]
+        <app-icon [size]="size()" [icon]="icon()" [class]="color()"
+            (mouseenter)="onMouseEnter()"
+            (mouseleave)="onMouseLeave()"/>
+        @if (shouldShow() > 0) {
+            <div class="tooltip card acrylic-card" animate.leave="leave"
+                (mouseenter)="onMouseEnter()"
+                (mouseleave)="onMouseLeave()">
+                <ng-content/>
+            </div>
+        }`,
+    styleUrl: './tooltip.scss'
 })
 export class TooltipComponent {
 
     protected readonly icon = input<Icon>('info');
     protected readonly color = input<ColorName>('accent');
-    protected readonly size = input<'xs' | 'sm' | 'smaller' | 'md' | 'lg' | 'xl'>('xs');
+    protected readonly size = input<IconSize>('xs');
+
+    protected readonly shouldShow = signal(0);
+
+    protected onMouseEnter() {
+        this.shouldShow.update(count => count + 1);
+    }
+
+    protected async onMouseLeave() {
+        await wait(500);
+        this.shouldShow.update(count => count - 1);
+    }
 }
