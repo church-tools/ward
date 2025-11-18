f695939bd2e53c845ec2d760a07ea6fad29f9ee1
Imma <imma.becker@outlook.de>
2025-11-18 17:11:49 +0100

    lcr link on members page

--- src/app/private/members-page.ts
+++ src/app/private/members-page.ts
@@ -6,12 +6,20 @@ import { Table } from '../modules/shared/table.types';
 import { xcomputed } from '../shared/utils/signal-utils';
 import { RowPageService } from './row-page.service';
 import { PrivatePageComponent } from './shared/private-page';
+import LinkButtonComponent from '../shared/form/button/link/link-button';
 
 @Component({
     selector: 'app-members-page',
     template: `
         <span class="h0">{{ 'MEMBERS_PAGE.TITLE' | translate }}</span>
         <p>Members component content goes here.</p>
+        <div class="row">
+            <app-link-button href="https://lcr.churchofjesuschrist.org/records/member-list"
+                icon="open"
+                [outside]="true" [newTab]="true" type="secondary">
+                <span outside>{{ 'MEMBERS_PAGE.LCR_MEMBER_PAGE' | translate }}</span>
+            </app-link-button>
+        </div>
         <app-row-card-list
             tableName="member"
             [getQuery]="getQuery"
@@ -20,7 +28,7 @@ import { PrivatePageComponent } from './shared/private-page';
             [getUrl]="getMemberUrl"
             [activeId]="activeMemberId()"/>
     `,
-    imports: [TranslateModule, RowCardListComponent],
+    imports: [TranslateModule, RowCardListComponent, LinkButtonComponent],
     host: { class: 'page narrow' },
 })
 export class MembersPageComponent extends PrivatePageComponent {
