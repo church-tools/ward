f6b71176d90bd6c570f7b2ca1d8b751bfdf24cc5
Imma <imma.becker@outlook.de>
2025-12-29 18:38:16 +0100

    Enhance user page functionality; add admin role management and self-check logic

--- src/app/private/users/user-page.ts
+++ src/app/private/users/user-page.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
+import { ProfileService } from '../../modules/profile/profile.service';
 import AsyncButtonComponent from '../../shared/form/button/async/async-button';
 import SwitchComponent from '../../shared/form/switch/switch';
+import { xcomputed } from '../../shared/utils/signal-utils';
 import { SyncedFieldDirective } from "../../shared/utils/supa-sync/synced-field.directive";
 import { RowHistoryComponent } from "../shared/row-history";
 import { RowPageComponent } from '../shared/row-page';
@@ -13,29 +15,33 @@ import { RowPageComponent } from '../shared/row-page';
         <app-switch label="{{ 'USER_PAGE.IS_ADMIN' | translate }}" [forceLabelOnTop]="true"
             [syncedRow]="syncedRow" column="is_admin" name="is_admin" [disabled]="true"/>
         <div class="row center-content gap-4">
-            @if (syncedRow.value()?.unit_approved) {
-                @if (syncedRow.value()?.is_admin) {
-                    <app-async-button icon="shield_dismiss"
-                        type="secondary"
-                        [onClick]="removeAdmin">
-                        {{ 'USER_PAGE.REMOVE_ADMIN' | translate }}
-                    </app-async-button>
+            @if (isSelf()) {
+
+            } @else {
+                @if (syncedRow.value()?.unit_approved) {
+                    @if (syncedRow.value()?.is_admin) {
+                        <app-async-button icon="shield_dismiss"
+                            type="secondary"
+                            [onClick]="setAdmin(false)">
+                            {{ 'USER_PAGE.REMOVE_ADMIN' | translate }}
+                        </app-async-button>
+                    } @else {
+                        <app-async-button icon="shield_person"
+                            type="secondary"
+                            [onClick]="setAdmin(true)">
+                            {{ 'USER_PAGE.MAKE_ADMIN' | translate }}
+                        </app-async-button>
+                    }
                 } @else {
-                    <app-async-button icon="shield_person"
-                        type="secondary"
-                        [onClick]="makeAdmin">
-                        {{ 'USER_PAGE.MAKE_ADMIN' | translate }}
+                    <app-async-button icon="checkmark" color="success"
+                        [onClick]="approve(true)">
+                        {{ 'USER_PAGE.APPROVE' | translate }}
+                    </app-async-button>
+                    <app-async-button icon="dismiss" color="danger"
+                        [onClick]="approve(false)">
+                        {{ 'USER_PAGE.REJECT' | translate }}
                     </app-async-button>
                 }
-            } @else {
-                <app-async-button icon="checkmark" color="success"
-                    [onClick]="approve(true)">
-                    {{ 'USER_PAGE.APPROVE' | translate }}
-                </app-async-button>
-                <app-async-button icon="dismiss" color="danger"
-                    [onClick]="approve(false)">
-                    {{ 'USER_PAGE.REJECT' | translate }}
-                </app-async-button>
             }
         </div>
         <app-row-history [row]="syncedRow.value()" class="mt-auto"/>
@@ -48,6 +54,11 @@ export class UserPageComponent extends RowPageComponent<'profile'> {
 
     protected readonly tableName = 'profile';
 
+    private readonly profileService = inject(ProfileService);
+
+    protected readonly isSelf = xcomputed([this.profileService.own, this.syncedRow.id],
+        (own, id) => own?.id === id);
+
     protected approve(approve: boolean) {
         return async () => {
             await this.supabase.callEdgeFunction('approve-user', { profile_id: this.syncedRow.id(), approve });
@@ -55,11 +66,9 @@ export class UserPageComponent extends RowPageComponent<'profile'> {
         }
     }
 
-    protected makeAdmin = async () => {
-        
-    }
-
-    protected removeAdmin = async () => {
-        
+    protected setAdmin(admin: boolean) {
+        return async () => {
+            await this.supabase.callEdgeFunction('set-user-admin', { profile_id: this.syncedRow.id(), set_admin: admin });
+        }
     }
 }
